cad9b5f8dc6af4c708ae12068c56296f3a0d1995
Aleixrei73 <aleix.armero73@gmail.com>
2025-12-06 03:20:10 +0100

    2 fet i 3 començat

--- lab1/utils.py
+++ lab1/utils.py
@@ -19,6 +19,24 @@ def line_draw(line, canv, size, color=(255,255,255)):
         end = (w, get_y(w))
     canv.line([beg, end], width=4, fill=color)
 
+def get_beg_end(line, size):
+    def get_y(t):
+        return -(line[0] * t + line[2]) / line[1]
+
+    def get_x(t):
+        return -(line[1] * t + line[2]) / line[0]
+
+    w, h = size
+
+    if line[0] != 0 and abs(get_x(0) - get_x(w)) < w:
+        beg = (get_x(0), 0)
+        end = (get_x(h), h)
+    else:
+        beg = (0, get_y(0))
+        end = (w, get_y(w))
+
+    return beg, end
+
 
 def plot_img(img, do_not_use=[0]):
     plt.figure(do_not_use[0])
